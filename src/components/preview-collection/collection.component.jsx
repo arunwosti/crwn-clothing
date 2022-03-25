989feed0b8805b2ee8066becbedf548712ffbd2d
arunwosti <ostiarun55@gmail.com>
2022-03-25 16:52:58 +0545

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/preview-collection/collection.component.jsx
+++ src/components/preview-collection/collection.component.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import './collection.style.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
+
+
 export default function CollectionPreview({title, items}) {
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
           {items
-          .filter((item,index)=>index<4)
-          .map(({id, ...otherItemProps}) => (
-            <CollectionItem key={id} {...otherItemProps}/>
+          .filter((item, index)=>index<4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item}/>
+            
           )) }
+          
         </div>
+        
     </div>
   )
 }
